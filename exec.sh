443d584308cba69be0b694a1a986070b1f980ce9
Roger Kowalewski <roger.kowalewski@gmail.com>
2014-10-16 23:48:13 +0200

    added minijava samples

--- exec.sh
+++ exec.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
-java -cp .:/Users/kowa/Develop/projects/praktikum_compiler/lib/java-cup-11a.jar straightline.Test example.sl
+if [ -z "$1" ]
+  then
+    echo "first argument must be a minijava program (see the minijava_examples directory)!";
+  else
+    java -cp .:/Users/kowa/Develop/projects/praktikum_compiler/lib/java-cup-11a.jar straightline.Test $1
+fi
