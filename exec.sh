f781e1d31c1257dbd2cff9f88a35a570ec4fd90c
Roger Kowalewski <kowalewski@cip.ifi.lmu.de>
2014-11-20 12:11:04 +0100

    restructured bash script to execute files

--- exec.sh
+++ exec.sh
@@ -1,28 +1,34 @@
 #!/bin/zsh
 
+DEST_DIR="./temp/";
+COMPILED_SUFFIX=".c";
+BINARY_SUFFIX=".out";
+RUNTIME="./tree2c/runtime.c";
+
 if [[ -f $1 ]];
   then
     FILE="$(basename "$1")";
-    java -cp target:/Users/kowa/Develop/projects/praktikum_compiler/lib/java-cup-11a.jar main.Test $1 >! ./temp/${FILE}.c;
+    COMPILED_FILE="${DEST_DIR}${FILE}${COMPILED_SUFFIX}";
+    java -cp target:./lib/java-cup-11a.jar main.Test $1 >! ${COMPILED_FILE};
     STATUS="${?}";
 
     if [[ ${STATUS} > 0 ]];
     then
-      cat ./temp/${FILE}.c;
-      rm ./temp/${FILE}.c
+      cat ${COMPILED_FILE};
+      rm ${COMPILED_FILE}
       exit 1;
     fi
 
-    OUT="./temp/${FILE}.out";
+    BINARY_FILE="${DEST_DIR}${FILE}${BINARY_SUFFIX}";
 
-    echo "Compiling the file ./temp/${FILE}.c";
+    echo "Compiling the file ${COMPILED_FILE}";
 
-    gcc -m32 -w -g -O0 -o $OUT "./temp/$FILE.c" ./tree2c/runtime.c;
+    gcc -m32 -w -g -O0 -o ${BINARY_FILE} ${COMPILED_FILE} ${RUNTIME};
 
-    if [[ -f $OUT ]];
+    if [[ -f ${BINARY_FILE} ]];
       then
-        echo "Executing the binary of $1...";
-        "./$OUT";
+        echo "Executing the binary...";
+        "./${BINARY_FILE}";
     fi
   else
     echo "first argument must be a minijava program (see the minijava_examples directory)!";
